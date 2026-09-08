e7d55377802b3d0dcd4e914fe2fe090392bceb20
Sharon-Mashai <mashaisharonmotsatsi@gmail.com>
2026-09-08 21:08:49 +0200

    Added an itemRoutes file that decides where a request goes.

---
+++ src/routes/itemRoutes.ts
@@ -0,0 +1,53 @@
+import type { IncomingMessage, ServerResponse } from "http";
+import { getItems, getItemById, createItem, updateItem, deleteItem,} from "../controllers/itemController.js";
+
+export const itemRoutes = (req: IncomingMessage, res: ServerResponse) => {
+  // GET /items
+  if (req.method === "GET" && req.url === "/items") {
+    getItems(res);
+    return;
+  }
+
+  // POST /items
+  if (req.method === "POST" && req.url === "/items") {
+    createItem(req, res);
+    return;
+  }
+
+  // Check routes that contain an item ID
+  if (req.url?.startsWith("/items/")) {
+    const id = Number(req.url.split("/")[2]);
+
+    // GET /items
+    if (req.method === "GET") {
+      getItemById(res, id);
+      return;
+    }
+
+    // PUT /items
+    if (req.method === "PUT") {
+      updateItem(req, res, id);
+      return;
+    }
+
+    // DELETE /items
+    if (req.method === "DELETE") {
+      deleteItem(res, id);
+      return;
+    }
+  }
+
+  // Route not found
+  res.writeHead(404, {
+    "Content-Type": "application/json",
+  });
+
+  res.end(
+    JSON.stringify({
+      error: {
+        status: 404,
+        message: "Route not found",
+      },
+    }),
+  );
+};
